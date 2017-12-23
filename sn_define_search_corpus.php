3a533dbb4dae077f32fe1a8ef88b7d2351d431c7
Alexander Trust <atrust@sajonara.de>
2017-12-23 04:36:50 +0100

    Define search corpus for Wordpress

---
+++ sn_define_search_corpus.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Add a function to to filter search results corpus */
+
+add_filter( 'pre_get_posts', 'sn_define_search_corpus' );
+  function sn_define_search_corpus( $query ) {
+    if ( $query->is_search ) {
+      $query->set( 'post_type', array( 'X', 'Y', 'Z' ) );
+     }
+    return $query;
+}
+
+/*
+ * Add this code into your theme's functions.php file.
+ * Rename X, Y and Z with the names of the custom post types you want to be added to your the search results.
+ * Don't forget to add posts or pages to the array, when you want them to be integrated too.
+ */
