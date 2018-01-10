9e35db55fe98adbc57c4a7f2540e80e00c607ce5
Alexander Trust <atrust@sajonara.de>
2018-01-10 14:09:59 +0100

    Limit search filter to frontend
    Found out that the function interferes with the global query in the backend too. So I added an condition to only use it in the frontend.

--- sn_define_search_corpus.php
+++ sn_define_search_corpus.php
@@ -10,6 +10,11 @@ add_filter( 'pre_get_posts', 'sn_define_search_corpus' );
     return $query;
 }
 
+/* Use filter only in the frontend, otherwise it interferes with backend search */
+if(!is_admin()) {
+  add_filter( 'pre_get_posts', 'sn_define_search_corpus' );
+}
+
 /*
  * Add this code into your theme's functions.php file.
  * Rename X, Y and Z with the names of the custom post types you want to be added to your the search results.
